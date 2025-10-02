616241a33edebf91bc80aed0b905d1161acde7fa
Rivelino <macienrivelino86@gmail.com>
2025-10-01 21:14:31 -0400

    deploy applications with lb (tk4023)

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "state-lock"
+    use_lockfile = false
+    //dynamodb_table = "state-lock"
   }
 }
