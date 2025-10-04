4089cab523090139eaf42deb92bc030576a466fd
Rivelino <macienrivelino86@gmail.com>
2025-10-04 16:15:11 -0400

    new deployment (tk1080)

--- alb.tf
+++ alb.tf
@@ -1,4 +1,78 @@
 
+# resource "aws_lb_target_group" "alb-target-group" {
+#   name     = "application-lb-tg"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = aws_vpc.vpc1.id
+
+#   health_check {
+#     enabled             = true
+#     healthy_threshold   = 3
+#     interval            = 10
+#     matcher             = 200
+#     path                = "/"
+#     port                = "traffic-port"
+#     protocol            = "HTTP"
+#     timeout             = 6
+#     unhealthy_threshold = 3
+#   }
+# }
+
+# resource "aws_lb_target_group_attachment" "attach-app1" {
+#   target_group_arn = aws_lb_target_group.alb-target-group.arn
+#   target_id        = aws_instance.server1.id
+#   port             = 80
+# }
+# resource "aws_lb_target_group_attachment" "attach-app" {
+#   target_group_arn = aws_lb_target_group.alb-target-group.arn
+#   target_id        = aws_instance.server2.id
+#   port             = 80
+# }
+
+# ## Listerner
+
+# resource "aws_lb_listener" "alb-http-listener1" {
+#   load_balancer_arn = aws_lb.application-lb.arn
+#   port              = 80
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.alb-target-group.arn
+#   }
+# }
+
+# # Create the load balancer
+
+# resource "aws_lb_listener" "alb-http-listener" {
+#   load_balancer_arn = aws_lb.application-lb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.alb-target-group.arn
+#   }
+# }
+# resource "aws_lb" "application-lb" {
+#   name               = "application-lb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.sg2.id]
+#   subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+
+#   enable_deletion_protection = false
+
+#   tags = {
+#     Environment = "application-lb"
+#     Name        = "Application-lb"
+
+#   }
+# }
+
+
+
+# Target Group
 resource "aws_lb_target_group" "alb-target-group" {
   name     = "application-lb-tg"
   port     = 80
@@ -9,7 +83,7 @@ resource "aws_lb_target_group" "alb-target-group" {
     enabled             = true
     healthy_threshold   = 3
     interval            = 10
-    matcher             = 200
+    matcher             = "200"
     path                = "/"
     port                = "traffic-port"
     protocol            = "HTTP"
@@ -18,42 +92,20 @@ resource "aws_lb_target_group" "alb-target-group" {
   }
 }
 
+# Target Group Attachments
 resource "aws_lb_target_group_attachment" "attach-app1" {
   target_group_arn = aws_lb_target_group.alb-target-group.arn
   target_id        = aws_instance.server1.id
   port             = 80
 }
-resource "aws_lb_target_group_attachment" "attach-app" {
+
+resource "aws_lb_target_group_attachment" "attach-app2" {
   target_group_arn = aws_lb_target_group.alb-target-group.arn
   target_id        = aws_instance.server2.id
   port             = 80
 }
 
-## Listerner
-
-resource "aws_lb_listener" "alb-http-listener1" {
-  load_balancer_arn = aws_lb.application-lb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb-target-group.arn
-  }
-}
-
-# Create the load balancer
-
-resource "aws_lb_listener" "alb-http-listener" {
-  load_balancer_arn = aws_lb.application-lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb-target-group.arn
-  }
-}
+# Application Load Balancer
 resource "aws_lb" "application-lb" {
   name               = "application-lb"
   internal           = false
@@ -66,8 +118,17 @@ resource "aws_lb" "application-lb" {
   tags = {
     Environment = "application-lb"
     Name        = "Application-lb"
-
   }
 }
 
+# Single HTTP Listener
+resource "aws_lb_listener" "alb-http-listener" {
+  load_balancer_arn = aws_lb.application-lb.arn
+  port              = 80
+  protocol          = "HTTP"
 
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-target-group.arn
+  }
+}
